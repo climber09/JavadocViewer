363741780a263c466995543028f3f0d75304b55d
Jim <hunter.james.p@gmail.com>
2021-11-19 03:59:23 -0800

    Modified content-type header handling.

--- src/main/java/com/hunterjdev/fsutil/FileServlet.java
+++ src/main/java/com/hunterjdev/fsutil/FileServlet.java
@@ -1,20 +1,16 @@
 package com.hunterjdev.fsutil;
 
 /*
- * This work is licensed under the Common Public Attribution License 
- * Version 1.0 (CPAL-1.0). To view a copy of this license, visit 
+ * This work is licensed under the Common Public Attribution License
+ * Version 1.0 (CPAL-1.0). To view a copy of this license, visit
  * http://opensource.org/licenses/CPAL-1.0.
- * 
+ *
  */
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -27,15 +23,14 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Used as a file server for local files that are outside of the application context.
- * 
+ *
  * @author James Hunter
- * 
+ *
  */
 @WebServlet(name="FileServlet", urlPatterns={"/file"})
-//@WLServlet(name="/FileServlet", mapping={"/file"})
 public class FileServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    
+
     public static final String ABS_FILE_PATH = "absolute.file.path";
     public static final String FILE_SERVE_URI = "/file";
 
@@ -44,9 +39,11 @@ public class FileServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String filePath = (String)request.getAttribute(ABS_FILE_PATH);
-        
-        if (filePath != null ) {
-            this.serveFile(request, response, filePath);
+        if (filePath != null) {
+            File file = new File(filePath);
+            if (file.exists() && file.canRead()) {
+                this.serveFile(request, response, file);
+            }
         }
     }
 
@@ -56,50 +53,48 @@ public class FileServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doGet(request, response);
     }
-    
-    protected void serveFile(HttpServletRequest request, HttpServletResponse response, String filePath) throws IOException {
-        String fileExt = filePath.substring(filePath.lastIndexOf('.') + 1);
 
-        if (fileExt.matches("gif|png|jpg|jpeg|bmp")) {
-            response.setContentType("image/".concat(fileExt));
+    protected void serveFile(HttpServletRequest request, HttpServletResponse response, File file) throws IOException {
+        String fileName = file.getName();
+        String fileExt = fileName.substring(fileName.lastIndexOf('.') + 1);
 
-            int length = -1;
-            int bufferSize = 1024 * 2;
-            byte[] buffer = new byte[bufferSize];
-
-            try(BufferedInputStream in =
-                    new BufferedInputStream( new FileInputStream(filePath) )) {
-                //                      SeekableByteChannel in = Files.newByteChannel(Paths.get(filePath))) {
-                ServletOutputStream out = response.getOutputStream();
-                while ((length = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, length);
-                }
-                in.close();
-                out.close();
-            }
+        switch (fileExt) {
+            case "gif": case "png": case "jpg": case "jpeg": case "bmp":
+                response.setContentType("image/".concat(fileExt));
+                break;
+            case "zip":
+                response.setContentType("application/zip");
+                break;
+            case "woff":
+                response.setContentType("font/woff");
+                break;
+            case "woff2":
+                response.setContentType("font/woff2");
+                break;
+            case "css":
+                response.setContentType("text/css;charset=utf-8");
+                break;
+            case "js":
+                response.setContentType("text/javascript;charset=utf-8");
+                break;
+            case "txt":
+                response.setContentType("text/plain;charset=utf-8");
+                break;
+            default:
+                response.setContentType("text/html;charset=utf-8");
         }
-        else {
-            if (fileExt.matches("htm|html")) {
-                response.setContentType("text/html");
-            } else if (fileExt.equals("css")) {
-                response.setContentType("text/css");
-            } else if (fileExt.equals("js")) {
-                response.setContentType("application/x-javascript");
-            }
-            try(BufferedReader reader =  
-                    Files.newBufferedReader(
-                            Paths.get(filePath), Charset.defaultCharset())) {
-                //                          new BufferedReader( new FileReader(filePath) )){
 
-                PrintWriter writer = response.getWriter();
-                String line = null;
+        int length = -1;
+        int bufferSize = 1024 * 2;
+        byte[] buffer = new byte[bufferSize];
 
-                while ((line = reader.readLine()) != null) {
-                    writer.println(line);
-                }
-                reader.close();
-                writer.close();
+        try( BufferedInputStream in = new BufferedInputStream( new FileInputStream(file) );
+             ServletOutputStream out = response.getOutputStream(); ) {
+            while ((length = in.read(buffer)) != -1) {
+                out.write(buffer, 0, length);
             }
+            in.close();
+            out.close();
         }
     }
 
